189badc70fbf2476c1f1fce9ca6d1808c84156f6
Sangjin Lee <leesj2004@gmail.com>
2022-11-13 18:23:44 +0900

    추가

--- loop.sh
+++ loop.sh
@@ -6,7 +6,11 @@ done
 
 
 #
+# 변수를 초기화할때 띄어쓰면 안된다. (변수명과 값 사이에 공백이 있으면 에러발생)
 #
+CNT=0
+
 for var in `ls *.sh`; do
-	echo ${var}	
+    CNT=$(echo "$CNT + 1" | bc)
+    printf "${var}: %02d, %s\n" `echo ${CNT}` `echo end`;
 done
