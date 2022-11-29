898b497e3f100ffed9cfa5247ddc3e8df45b023e
Sangjin Lee <leesj2004@gmail.com>
2022-11-29 10:37:09 +0900

    initial

---
+++ kvision_device/log.sh
@@ -0,0 +1,14 @@
+SRCFILE=/var/log/message
+
+DESTDIR=/mnt/dev2/tmp/
+DESTFILE=message_`date +"%m%d_%H%M"`
+
+if [ ! -d $DESTDIR ] ; then
+        echo need to mount
+        echo
+        exit
+fi
+
+cp -v $SRCFILE $DESTDIR/$DESTFILE
+echo
+
