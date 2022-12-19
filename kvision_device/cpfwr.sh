a4787ca364aeb6f9d50af4d9612164d568da871a
Sangjin Lee <leesj2004@gmail.com>
2022-12-19 15:34:51 +0900

    initial

---
+++ kvision_device/cpfwr.sh
@@ -0,0 +1,28 @@
+SRCDIR=/mnt/dev2/tmp
+SRCFILE=$1
+
+DESTDIR=/dvr
+DESTFILE=fwr
+
+if [ ! -f $SRCDIR/$SRCFILE ] ; then
+        echo need to mount
+        echo
+        exit
+fi
+
+echo
+echo $SRCDIR/$SRCFILE
+echo
+
+echo -n "Copy? "
+read input
+
+if [ "${input}" = "" ]; then
+        echo $SRCDIR/$SRCFILE $DESTDIR/$DESTFILE
+        cp -v $SRCDIR/$SRCFILE $DESTDIR/$DESTFILE
+
+        echo
+        md5sum $DESTDIR/$DESTFILE
+        echo
+fi
+
