2a6fb22cb0ea4209ba433d9bcaf84156d23bfb0b
Sangjin Lee <leesj2004@gmail.com>
2022-12-22 10:48:05 +0900

    여러 모델 지원하도록 수정

--- kvision_ubuntu/get.sh
+++ kvision_ubuntu/get.sh
@@ -1,5 +1,17 @@
+TOPDIR=/home/ubuntu/nt98336
+#TOPDIR=/home/ubuntu/nt9831x
+
+SRCDIR=${TOPDIR}/aries/cal
 DIRS="ara  auriga  common-dvr-2mp  crater  crux  fornax  generic  indus  libra  mensa  pavo  puppis  volans"
-SRCDIR=~/nt9831x/aries/cal
+
+CANIS=canis
+CANIS_DIR=${TOPDIR}/aries/cal/${CANIS}
+
+case ${TOPDIR#/home/ubuntu/} in
+	nt9831x) FILES="_chip_nt9832x.cpp _chip_nt98336.cpp" ;;
+	nt9832x) FILES="_chip.cpp _chip_nt98336.cpp" ;;
+	nt98336) FILES="_chip.cpp _chip_nt9832x.cpp" ;;
+esac
 
 
 
@@ -7,6 +19,13 @@ for DIR in ${DIRS} ; do
 	if [ -d ${SRCDIR}/${DIR} ]; then
 		mv -v ${SRCDIR}/${DIR} .
 	fi
+done
+
+mkdir -p ./${CANIS}
+for FILE in ${FILES} ; do
+        if [ -f ${CANIS_DIR}/${FILE} ]; then
+                mv -v ${CANIS_DIR}/${FILE} ./${CANIS}
+        fi
 done
 
 exit 0
--- kvision_ubuntu/put.sh
+++ kvision_ubuntu/put.sh
@@ -1,5 +1,18 @@
+TOPDIR=/home/ubuntu/nt98336
+#TOPDIR=/home/ubuntu/nt9831x
+
+DESTDIR=${TOPDIR}/aries/cal
 DIRS="ara  auriga  common-dvr-2mp  crater  crux  fornax  generic  indus  libra  mensa  pavo  puppis  volans"
-DESTDIR=~/nt9831x/aries/cal
+
+CANIS=canis
+CANIS_DIR=${TOPDIR}/aries/cal/${CANIS}
+FILES="_chip.cpp _chip_nt9832x.cpp"
+
+case ${TOPDIR#/home/ubuntu/} in
+	nt9831x) FILES="_chip_nt9832x.cpp _chip_nt98336.cpp" ;;
+	nt9832x) FILES="_chip.cpp _chip_nt98336.cpp" ;;
+	nt98336) FILES="_chip.cpp _chip_nt9832x.cpp" ;;
+esac
 
 
 
@@ -8,6 +21,13 @@ for DIR in ${DIRS} ; do
 		mv -vf ${DIR} ${DESTDIR}
 	fi
 done
+
+for FILE in ${FILES} ; do
+        if [ -f ${CANIS}/${FILE} ]; then
+                mv -v ${CANIS}/${FILE} ${CANIS_DIR}
+        fi
+done
+rmdir ${CANIS}
 
 exit 0
 
