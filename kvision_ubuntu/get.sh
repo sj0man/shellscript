5a6333f641c334e97f8e0a3b9fa4e3c67790dca7
Sangjin Lee <leesj2004@gmail.com>
2022-12-26 20:41:48 +0900

    변수 선언 변경

--- kvision_ubuntu/get.sh
+++ kvision_ubuntu/get.sh
@@ -1,18 +1,16 @@
-TOPDIR=/home/ubuntu/nt98336
 #TOPDIR=/home/ubuntu/nt9831x
+#TOPDIR=/home/ubuntu/nt9832x
+TOPDIR=/home/ubuntu/nt98336
+
+#FILES="_chip_nt9832x.cpp _chip_nt98336.cpp"
+#FILES="_chip.cpp _chip_nt98336.cpp"
+FILES="_chip.cpp _chip_nt9832x.cpp"
 
 SRCDIR=${TOPDIR}/aries/cal
 DIRS="ara  auriga  common-dvr-2mp  crater  crux  fornax  generic  indus  libra  mensa  pavo  puppis  volans"
 
 CANIS=canis
 CANIS_DIR=${TOPDIR}/aries/cal/${CANIS}
-
-case ${TOPDIR#/home/ubuntu/} in
-	nt9831x) FILES="_chip_nt9832x.cpp _chip_nt98336.cpp" ;;
-	nt9832x) FILES="_chip.cpp _chip_nt98336.cpp" ;;
-	nt98336) FILES="_chip.cpp _chip_nt9832x.cpp" ;;
-esac
-
 
 
 for DIR in ${DIRS} ; do
--- kvision_ubuntu/put.sh
+++ kvision_ubuntu/put.sh
@@ -1,5 +1,10 @@
-TOPDIR=/home/ubuntu/nt98336
 #TOPDIR=/home/ubuntu/nt9831x
+#TOPDIR=/home/ubuntu/nt9832x
+TOPDIR=/home/ubuntu/nt98336
+
+#FILES="_chip_nt9832x.cpp _chip_nt98336.cpp"
+#FILES="_chip.cpp _chip_nt98336.cpp"
+FILES="_chip.cpp _chip_nt9832x.cpp"
 
 DESTDIR=${TOPDIR}/aries/cal
 DIRS="ara  auriga  common-dvr-2mp  crater  crux  fornax  generic  indus  libra  mensa  pavo  puppis  volans"
@@ -7,13 +12,6 @@ DIRS="ara  auriga  common-dvr-2mp  crater  crux  fornax  generic  indus  libra  
 CANIS=canis
 CANIS_DIR=${TOPDIR}/aries/cal/${CANIS}
 FILES="_chip.cpp _chip_nt9832x.cpp"
-
-case ${TOPDIR#/home/ubuntu/} in
-	nt9831x) FILES="_chip_nt9832x.cpp _chip_nt98336.cpp" ;;
-	nt9832x) FILES="_chip.cpp _chip_nt98336.cpp" ;;
-	nt98336) FILES="_chip.cpp _chip_nt9832x.cpp" ;;
-esac
-
 
 
 for DIR in ${DIRS} ; do
