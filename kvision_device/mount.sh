7ec9bf02cbbb967f7f39aae0fea9abb9884e3ff0
Sangjin Lee <leesj2004@gmail.com>
2022-12-26 15:31:16 +0900

    폴더 만들기

--- kvision_device/mount.sh
+++ kvision_device/mount.sh
@@ -2,8 +2,15 @@
 #       check NFS directory in /etc/exports
 #
 
+HOME=/home/ubuntu
+MNT_DIR=/mnt/dev2
+IPADDR=192.168.0.21
+
+if [ -d ${MNT_DIR} ] ; then
+    mkdir -p /mnt/dev2
+fi
 
 # ifconfig eth0 hw ether 00:09:1b:00:50:08
 udhcpc -i eth0
-mount -t nfs -o nolock 192.168.0.21:/home/ubuntu/ /mnt/dev2
+mount -t nfs -o nolock ${IPADDR}:${HOME} ${MNT_DIR}
 
