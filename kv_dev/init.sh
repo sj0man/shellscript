e7b8b2b3132ceed37cc1dc153cd831bb0670a2ad
Sangjin Lee <leesj2004@gmail.com>
2023-01-11 11:13:44 +0900

    shellscript 복사 추가

--- kv_dev/init.sh
+++ kv_dev/init.sh
@@ -1,13 +1,21 @@
+MNTDIR=/mnt/dev2
+SHDIR=${MNTDIR}/mylab/shellscript/kv_dev
+
 LOG_FILE=/var/log/message
 LOG_LINK=var
+
 CPLIB=cplib.sh
 LIBDAL=libdal.sh
 
+if [ -d ${SHDIR} ] ; then
+    cp -v ${SHDIR}/* ${PWD}
+fi
+
 if [ ! -f ${LOG_LINK} ] ; then
-    ln -s ${LOG_FILE} ${LOG_LINK}
+    ln -sv ${LOG_FILE} ${LOG_LINK}
 fi
 
 if [ ! -f ${LIBDAL} ] ; then
-    ln -s ${CPLIB} ${LIBDAL}
+    ln -sv ${CPLIB} ${LIBDAL}
 fi
 
