bbd0360f2b62442f9f818c38c7ee8a55dcf6ce60
Sangjin Lee <leesj2004@gmail.com>
2023-03-24 13:27:24 +0900

    initial

---
+++ kv_dev/cpdvr.sh
@@ -0,0 +1,39 @@
+SRCDIR=/mnt/dev2/nt9832x/crater/nt98633/dvr
+SRCFILE=dvr.ko
+
+DESTDIR=/dvr
+DESTFILE=dvr.ko
+
+if [ ! -f $SRCDIR/$SRCFILE ] ; then
+	echo need to mount
+	echo
+	exit
+fi
+
+echo
+md5sum $SRCDIR/$SRCFILE
+if [ -f $DESTDIR/$DESTFILE ] ; then
+	md5sum $DESTDIR/$DESTFILE
+fi
+echo
+
+echo -n "Copy? "
+read input
+
+if [ "${input}" = "" ]; then
+
+	cp -v $SRCDIR/$SRCFILE $DESTDIR/$DESTFILE
+
+	echo
+	md5sum $SRCDIR/$SRCFILE $DESTDIR/$DESTFILE
+	echo
+else
+	exit
+fi
+
+echo -n "Reboot? "
+read input
+
+if [ "${input}" = "" ]; then
+	reboot
+fi
