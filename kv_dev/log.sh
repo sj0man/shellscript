b5ebe8819ffeb2e36350189332ab964de6a91ce8
Sangjin Lee <leesj2004@gmail.com>
2023-05-16 10:15:53 +0900

    change filename

--- kv_dev/log.sh
+++ kv_dev/log.sh
@@ -1,7 +1,7 @@
 SRCFILE=/var/log/message
 
-DESTDIR=/mnt/dev2/tmp/
-DESTFILE=message_`date +"%m%d_%H%M"`
+DESTDIR=/mnt/dev2/tmp
+DESTFILE=`date +"%m%d-%H%M"`.log
 
 if [ ! -d $DESTDIR ] ; then
 	echo need to mount
