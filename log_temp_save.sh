d4913419dee0aabd9e45a0604531819c462f6c52
Sangjin Lee <leesj2004@gmail.com>
2025-07-08 09:52:04 +0900

    함수 구분, 백업 삭제

--- log_temp_save.sh
+++ log_temp_save.sh
@@ -1,37 +1,41 @@
 #!/bin/sh
 
 COUNT=0
-DELAY_SECOND=600 # Default delay is 10 minutes
+DELAY_SECOND=$((60 * 60))
 CHECK_INTERVAL=$((24 * 3600 / $DELAY_SECOND))
+DELETE_LINES=$(($CHECK_INTERVAL * 30))
 MAX_LINES=$(($CHECK_INTERVAL * 365))
 CPU_TEMP_FILE=/sys/class/thermal/thermal_zone0/temp
 LOGFILE=/var/log/cpu_temperature.log
-BAKFILE=/dvr/cpu_temperature.log
-CUR_DATE=$(date '+%Y-%m-%d')
 
-
-if [ -z $CPU_TEMP_FILE ]; then
-	exit
-fi
+CPU_TEMP=""
+HDD_TEMP=""
 
 [ -d /var/log ] || mkdir -p /var/log
 
-if [ -f "$BAKFILE" ]; then
-	cp "$BAKFILE" "$LOGFILE"
-fi
+get_cpu_temp()
+{
+	if [ -f "$CPU_TEMP_FILE" ]; then
+		CPU_TEMP="cpu=$(cat "$CPU_TEMP_FILE")"
+	else
+		CPU_TEMP="cpu=N/A"
+	fi
+}
 
-while true
-do
+get_hdd_temp()
+{
 	HDD_TEMP=""
 	for DISK in /dev/sd?; do
 		[ -b "$DISK" ] || continue
 
 		if smartctl -i "$DISK" | grep -q "SMART support is: Disabled"; then
-			smartctl -s on "$DISK"
+			smartctl -s on "$DISK" > /dev/null
+			sleep 1
+			TMP_TEMP=$(smartctl -A "$DISK" 2> /dev/null | awk '$1 == 194 {print $10}')
+			smartctl -s off "$DISK" > /dev/null
+		else
+			TMP_TEMP=$(smartctl -A "$DISK" 2> /dev/null | awk '$1 == 194 {print $10}')
 		fi
-
-		TMP_TEMP=""
-		TMP_TEMP=$(smartctl -A "$DISK" 2> /dev/null | awk '$1 == 194 {print $10}')
 
 		if [ -n "$TMP_TEMP" ]; then
 			#NAME=$(basename "$DISK")
@@ -39,28 +43,29 @@ do
 			HDD_TEMP="${HDD_TEMP}${NAME}=${TMP_TEMP} "
 		fi
 	done
+}
 
-
-	CPU_TEMP=$(cat $CPU_TEMP_FILE)
-
-	echo "$(date '+%Y-%m-%d-%H:%M:%S'): cpu=${CPU_TEMP} ${HDD_TEMP}" >> "$LOGFILE"
-	sleep $DELAY_SECOND
-
+keep_log_size()
+{
 	COUNT=$((COUNT + 1))
 	if [ "$COUNT" -ge "$CHECK_INTERVAL" ]; then
 		CURRENT_LINES=$(wc -l < "$LOGFILE")
 
 		if [ "$CURRENT_LINES" -gt "$MAX_LINES" ]; then
-			mv "${LOGFILE}" "${BAKFILE}_$(date '+%Y-%m-%d')"
-			touch "${LOGFILE}"
+			tail -n $(($MAX_LINES - $DELETE_LINES)) "$LOGFILE" > "${LOGFILE}.tmp"
+			mv "${LOGFILE}.tmp" "$LOGFILE"
 		fi
 		COUNT=0
 	fi
+}
 
-	NEW_DATE=$(date '+%Y-%m-%d')
-	if [ "$CUR_DATE" != "$NEW_DATE" ]; then
-		cp "${LOGFILE}" "${BAKFILE}"
-		CUR_DATE="$NEW_DATE"
-	fi
+while true
+do
+	get_cpu_temp
+	get_hdd_temp
 
+	echo "$(date '+%Y-%m-%d-%H:%M:%S'): ${CPU_TEMP} ${HDD_TEMP}" >> "$LOGFILE"
+	sleep $DELAY_SECOND
+
+	keep_log_size
 done
