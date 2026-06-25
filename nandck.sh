07e868d70c073e671dc88c412c00675630687ae8
Sangjin Lee <leesj2004@gmail.com>
2026-06-25 12:42:08 +0900

    개선

--- nandck.sh
+++ nandck.sh
@@ -3,9 +3,11 @@
 COUNT=0
 INFILE=/dev/urandom
 OUTFILE=/mnt/test.bin
+LOGFILE=/dvr/nandck.log
+ECCFILE=/sys/class/mtd/mtd4/ecc_failures
 
 # Remove old log
-dmesg -c >/dev/null 2>&1
+#dmesg -c | grep -iE "crc|ecc|uncorrectable|bitflip|jffs2.*error|jffs2.*crc|read error|write error|erase error" >> $LOGFILE
 
 if [ -f $OUTFILE ]; then
     rm -f $OUTFILE
@@ -14,31 +16,53 @@ fi
 while true
 do
     COUNT=$((COUNT + 1))
+    DATE_TIME=$(date +"%m%d-%H%M%S")
 
-    echo "===== LOOP ${COUNT} ====="
+    echo "===== ${DATE_TIME} ===== LOOP ${COUNT} =====" >> $LOGFILE
 
-    dd if=$INFILE of=$OUTFILE bs=1M count=100 conv=fsync >/dev/null 2>&1
+    # write random data to nand
+    #dd if=$INFILE of=$OUTFILE bs=1M count=100 conv=fsync 2>&1 | tee -a $LOGFILE >/dev/null
+	dd if=$INFILE of=$OUTFILE bs=1M count=100 conv=fsync >/dev/null 2>&1
 
     RET=$?
     if [ $RET -ne 0 ]; then
-        echo "dd failed (ret=$RET)"
+        echo "dd failed (ret=$RET)" >> $LOGFILE
     fi
     sync
+
+    # read back and check checksum
+    sha256sum $OUTFILE >/dev/null
+    cat $OUTFILE >/dev/null
+    sync
+
+    # Check new dmesg
+    ERRLOG=$(dmesg -c | grep -iE "crc|ecc|uncorrectable|bitflip|jffs2.*error|jffs2.*crc|read error|write error|erase error")
+    echo "$ERRLOG" >> $LOGFILE
+
+    if [ -n "$ERRLOG" ]; then
+        echo "" >> $LOGFILE
+        echo "########################################" >> $LOGFILE
+        echo "# NAND/JFFS2 ERROR DETECTED" >> $LOGFILE
+        echo "########################################" >> $LOGFILE
+        echo "$ERRLOG" >> $LOGFILE
+        echo "" >> $LOGFILE
+    fi
 
     rm -f $OUTFILE
     sync
 
-    # Check new dmesg
-    ERRLOG=$(dmesg -c | grep -iE \
-        "crc|ecc|uncorrectable|bitflip|jffs2.*error|jffs2.*crc|read error|write error|erase error")
+	ECCLOG=$(cat $ECCFILE)
+	echo "ECC Failures: $ECCLOG" >> $LOGFILE
 
-    if [ -n "$ERRLOG" ]; then
-        echo ""
-        echo "########################################"
-        echo "# NAND/JFFS2 ERROR DETECTED"
-        echo "########################################"
-        echo "$ERRLOG"
-        echo ""
-    fi
+	if [ "$ECCLOG" -gt 0 ]; then
+		echo "" >> $LOGFILE
+		echo "########################################" >> $LOGFILE
+		echo "# NAND ECC ERROR DETECTED" >> $LOGFILE
+		echo "########################################" >> $LOGFILE
+		echo "ECC Failures: $ECCLOG" >> $LOGFILE
+		echo "" >> $LOGFILE
+	fi
+
+	sleep 1
 done
 
